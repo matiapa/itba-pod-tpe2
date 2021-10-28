07e14518de43b49744370c54bad7c0e7f23d410a
Santiago <smonjeau@itba.edu.ar>
2021-10-28 15:03:41 -0300

    adapt test for query 3

--- client/src/test/java/Query3Test.java
+++ client/src/test/java/Query3Test.java
@@ -9,6 +9,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import pod.client.Query3;
+import pod.models.Pair;
 import pod.models.Tree;
 
 import java.util.*;
@@ -23,19 +24,20 @@ public class Query3Test {
 
 
     private final List<Tree> treeList = Arrays.asList(
-            new Tree("PEPE","LINIERS","calle123"),
-            new Tree("PEPE1","LINIERS","calle123"),
-            new Tree("PEPE2","LINIERS","calle123"),
-            new Tree("PEPE3","VILLA LURO","calle123"),
-            new Tree("PEPE4","VILLA LURO","calle123"),
-            new Tree("PEPE5","CABALLITO","calle123"),
-            new Tree("PEPE6","PALERMO","calle123"),
-            new Tree("PEPE7","COLEGIALES","calle123"),
-            new Tree("PEPE8","COLEGIALES","calle123")
+            new Tree("PEPE", "LINIERS", "calle123"),
+            new Tree("PEPE1", "LINIERS", "calle123"),
+            new Tree("PEPE2", "LINIERS", "calle123"),
+            new Tree("PEPE3", "VILLA LURO", "calle123"),
+            new Tree("PEPE4", "VILLA LURO", "calle123"),
+            new Tree("PEPE5", "CABALLITO", "calle123"),
+            new Tree("PEPE6", "PALERMO", "calle123"),
+            new Tree("PEPE7", "COLEGIALES", "calle123"),
+            new Tree("PEPE8", "COLEGIALES", "calle123")
 
-            );
+    );
+
     @Before
-    public void setUp (){
+    public void setUp() {
 
         hazelcastFactory = new TestHazelcastFactory();
         // Group Config
@@ -50,49 +52,38 @@ public class Query3Test {
     }
 
 
-
     @Test
     public void testMapReduceResult() throws ExecutionException, InterruptedException {
         String listName = "trees";
         IList<Tree> testListFromMember = member.getList(listName);
         testListFromMember.addAll(treeList);
         IList<Tree> testList = client.getList(listName);
-    }
-        /*
-        Map<String,Integer> mapRedResult = Query3.getMapReduceResult(client,testList);
 
 
-        Assert.assertEquals(3, (int) mapRedResult.get("LINIERS"));
-        Assert.assertEquals(2, (int) mapRedResult.get("VILLA LURO"));
-        Assert.assertEquals(1, (int) mapRedResult.get("CABALLITO"));
-        Assert.assertEquals(1, (int) mapRedResult.get("PALERMO"));
-        Assert.assertEquals(2, (int) mapRedResult.get("COLEGIALES"));
+        SortedSet<Pair<String, Integer>> mapRedResult = Query3.getMapReduceResult(client, testList);
 
-    }
-
-    @Test
-    public void testOrder(){
-        Map<String,Integer> resultToOrder = new HashMap<>();
-        resultToOrder.put("Bruno",3);
-        resultToOrder.put("Santi",5);
-        resultToOrder.put("Mati",4);
-        resultToOrder.put("Iña",5);
-
-        List<String> expectedNames = Arrays.asList("Iña","Santi","Mati","Bruno");
-        List<Integer> expectedInts = Arrays.asList(5,5,4,3);
+        List<Pair<String, Integer>> expectedResult = Arrays.asList(
+                new Pair<>("LINIERS", 3),
+                new Pair<>("COLEGIALES", 2),
+                new Pair<>("VILLA LURO", 2),
+                new Pair<>("CABALLITO", 1),
+                new Pair<>("PALERMO", 1)
+        );
 
         AtomicInteger i = new AtomicInteger();
-        Query3.getResultSorted(resultToOrder).forEach(r->{
-            Assert.assertEquals(expectedNames.get(i.get()),r.getKey());
-            Assert.assertEquals(expectedInts.get(i.get()),r.getValue());
+        mapRedResult.forEach(r -> {
+            Assert.assertEquals(expectedResult.get(i.get()).getLeft(), r.getLeft());
+            Assert.assertEquals(expectedResult.get(i.get()).getRight(), r.getRight());
             i.getAndIncrement();
         });
-    }
 
-         */
+
+    }
 
     @After
     public void closeHazelcast() {
         hazelcastFactory.shutdownAll();
+
     }
+
 }
